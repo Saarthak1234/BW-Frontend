5185f3e68ff1a8579947dc42a329a5fefbcb1581
saarthak1234 <saarthakagarwal0408@gmail.com>
2025-07-29 14:24:27 +0530

    Add google analytics to see the number of users

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
+import GoogleAnalytics from "@/components/GoogleAnalytics/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-br from-amber-50 via-yellow-50 to-amber-100 min-h-screen`}
       >
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
